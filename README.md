800093a78943cd97c478fb7db470d617b881aca9
Sergey Berezin <update.berezin@gmail.com>
2019-07-02 20:39:58 +0400

    Don't forget to schedule the cron

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# openshift-annotated-remover
-OpenShift annotated test-projects remover. Bash script, such gets list of projects older or equal 24h with some annotation and then deletes these ones. But don't forget set the default project annotation with the OpenShift
+# Remover of unpaid OpenShift projects 
+
+**Preparing OpenShift environment**:
+
+1. Choosing of default project
+`#oc project default` <br />
+
+2. Creating of service account such'll provide access to cluster and permissions for oc commands
+`#oc create sa unpaid` 
+
+3. Set permissions for SA
+`#oc adm policy add-cluster-role-to-user cluster-admin -z unpaid -n default` 
+
+4. Describe sa to determine token name
+`#oc describe sa unpaid` 
+
+5. Determine token-string
+`#oc describe secret unpaid-token-<from-output-of-previous-command>` 
+
+6. Place token-key inside the script using command: 
+`#oc login https://<link_to_oc>:8443 --insecure-skip-tls-verify=true --token`*
+
+7. Determine full path to oc bin in variable 
+ `$oc\_path`
+
+<br />
+
+***Example of token:** 
+
+> eyJhbGciOiJS<--very-long-string-->9jaXBdGVsDDapRXC3bp4Ix1rZVTGw
+
+---
+**Script start with CLI**:
+
+Just start bash script when all requirements are meet the conditions
+`#./delete.sh` 
+
+If you'd like to try script in new environment you may modify command inside `'out'` variable form `'oc delete project'` to `'oc get project'`
---
+++ delete.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#Path to oc bin (determine full oc bin path for crontab)
+oc_path="/usr/local/bin/oc"
+
+#Need to replace below string with SA token for API authorize
+${oc_path} login https://openshift.test.cloud:8443 --insecure-skip-tls-verify=true --token XWMId2HlZtp18g > /dev/null
+
+#For with list of projects in oc cluster and substitution all over the spaces with # character 
+for name in `${oc_path} get namespace -o template --template='{{ range .items }}{{ .metadata.name }} {{ index .metadata "annotations" "someannotations/replace_it" }} {{ .metadata.creationTimestamp}} {{ "\n" }} {{ end }}'| sed 's/ /#/g'`;
+do
+  #Trimming "date" without Z character for convert it at the next step
+  date=$(echo $name | sed 's/^\(.*\).$/\1/' | awk -F# '{print $NF}' | sed 's/Z//g');
+  
+  #Calculating current "date"
+  cur_date=`date -u '+%s'`
+  
+  #Convert "date" of existing projects
+  prj_date=`date -u -d "$date" '+%s'`
+  
+  #Diff between existing time and project life time
+  time=$(( $cur_date - $prj_date  ))
+  
+  #Grep UNPAID annotation & set of unpaid project ttl
+  if [[ "$name" =~ "UNPAID" && ${time} -ge 86400 ]];then
+
+  #Reverse substitution of spaces for correct using of awk
+  rm_prj=$(echo $name | sed 's/#/ /g' | awk '{print $1}');
+
+  #Total remove of unpaid projects and writing deletion time corresponding prj_name into logfile
+  out=`${oc_path} delete project $rm_prj`
+  result="${out} at `date -u '+%H:%M:%S UTC time %Y-%m-%d'` | Project age was ${time} seconds" 
+    if [[ "$out" != "" ]];then 
+      echo $result >> /var/log/deleted_unpaid_projects.log
+    fi
+   fi
+ done
+
